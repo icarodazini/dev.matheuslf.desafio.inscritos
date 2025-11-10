991a84474f1ea362d09674492a8f0c0e5331ce86
Icaro Dazini <icarodazini7@gmail.com>
2025-11-10 15:52:15 -0300

    feat/ adiciona GlobalExceptionHandler para implementar o tratamento global

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package dev.matheuslf.desafio.inscritos.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
