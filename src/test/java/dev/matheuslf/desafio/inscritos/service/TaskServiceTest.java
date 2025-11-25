f2ae72984387bc2201769a3ecca0f550d12493f3
Icaro Dazini <icarodazini7@gmail.com>
2025-11-25 10:36:41 -0300

    test: adiciona teste de criacao de tarefa com ID de projeto valido

---
+++ src/test/java/dev/matheuslf/desafio/inscritos/service/TaskServiceTest.java
@@ -0,0 +1,60 @@
+package dev.matheuslf.desafio.inscritos.service;
+
+import dev.matheuslf.desafio.inscritos.model.Project;
+import dev.matheuslf.desafio.inscritos.model.Task;
+import dev.matheuslf.desafio.inscritos.repository.ProjectRepository;
+import dev.matheuslf.desafio.inscritos.repository.TaskRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private ProjectRepository projectRepository;
+
+    @InjectMocks
+    private TaskService taskService;
+
+    private Project project;
+    private Task task;
+    private final Long idValido = 1L;
+    private final Long idInvalido = 74L;
+
+    @BeforeEach
+    public void setUp() {
+        project = new Project("Test Project", null);
+        project.setId(idValido);
+
+        task = new Task();
+        task.setId(idValido);
+        task.setTitle("Tarefa Mock");
+    }
+
+    @Test
+    @DisplayName("Teste de criação de tarefa com ID de projeto válido")
+    public void createTask_ValidProjectID_ReturnsSavedTask() {
+        when(projectRepository.findById(idValido)).thenReturn(Optional.of(project));
+
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        Task result = taskService.createTask(task, idValido);
+
+        Assertions.assertNotNull(result);
+
+        verify(taskRepository, times(1)).save(task);
+    }
+}
