6d42b1bf64ac339f552fa32a4d2c8d3e3192e4a9
Icaro Dazini <icarodazini7@gmail.com>
2025-12-23 09:10:25 -0300

    feat(exceptions): aumenta o GlobalExceptionHandler com tratamento de validação e erros genéricos

--- src/main/java/dev/matheuslf/desafio/inscritos/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/exceptions/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package dev.matheuslf.desafio.inscritos.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +15,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleGenericRuntimeException(RuntimeException e) {
+        return new ResponseEntity<>("Ocorreu um erro inesperado: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
